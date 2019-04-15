16801e5ef5020e93b3f5c46564f4fc47967ff2a9
Lynsey Powell <lynseympowell@gmail.com>
2019-04-14 20:22:27 -0600

    Add dependencies and create Post route

--- server.js
+++ server.js
@@ -1,1 +1,20 @@
 'use strict';
+
+//import required modules
+const express = require('express');
+const  bodyParser = require('body-parser');
+
+const jsonParser = bodyParser.json();
+
+//initialize a new express app
+const app = express();
+
+//import config values
+const { PORT } = require('./config');
+
+//Endpoint that accepts fragment data and returns uvt
+app.post('/uvt', jsonParser, (req, res) => {
+
+})
+
+app.listen(PORT, () => console.log(`App is listening on port ${PORT}`));
