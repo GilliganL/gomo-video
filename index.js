19fc029fbc6a6eeffe75def80741ebab4ebdae15
Lynsey Powell <lynseympowell@gmail.com>
2019-04-14 19:46:01 -0600

    Add Fragment object

---
+++ index.js
@@ -0,0 +1,27 @@
+//Fragment Data presented in JSON
+
+const fragments = {
+    "videos": [
+        {
+            "id": 1,
+            "fragments": [
+                {"start" : 0,"stop": 154839},
+                {"start": 68243, "stop": 300145},
+                {"start": 260394, "stop": 356724},
+                {"start": 450235, "stop": 592954},
+                {"start": 68243, "stop": 122395}
+            ]
+        },
+        {
+            "id": 2,
+            "fragments": [
+                {"start" : 0,"stop": 54839},
+                {"start": 68243, "stop": 75145},
+                {"start": 130394, "stop": 185724},
+                {"start": 10235, "stop": 25954}
+            ]
+        }
+    ]
+}
+
+//Send fragments to /api/uvt endpoint or process here?
